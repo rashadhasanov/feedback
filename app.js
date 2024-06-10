d682914960f2cec7c45335b9ede44d1bcbd0440c
Rashad Hasan <rashadhasandev@gmail.com>
2024-06-10 19:46:38 +0400

    second commit

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-const container = document.querySelector(".container");
-const button = document.querySelector("button");
-const feedback = document.querySelector(".feedback");
+const feedbackContainer = document.querySelector(".feedback-container");
+const sendFeedback = document.querySelector("#send-feedback");
+const feedbackMessage = document.querySelector(".feedback-message");
+const emojiContainer = document.querySelector(".emoji-container");
+const ratings = document.querySelectorAll(".raiting");
+const feedbackSmile = document.querySelector(".feedback-smile");
 
-button.addEventListener("click", () => {
-  container.classList.add("hidden");
-  feedback.classList.remove("hidden");
+let selectedRating;
+
+emojiContainer.addEventListener("click", (e) => {
+  ratings.forEach((item) => {
+    item.classList.remove("active");
+  });
+  selectedRating = e.target.closest(".raiting");
+  selectedRating.classList.add("active");
+});
+
+sendFeedback.addEventListener("click", () => {
+  feedbackContainer.classList.add("hidden");
+  feedbackMessage.classList.remove("hidden");
+  feedbackSmile.textContent = `Feddback:${selectedRating.textContent}`;
 });
